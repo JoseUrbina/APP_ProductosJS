57ab4028c1e57b6577cca0d6ecf981233e39a9ab
JoseUrbina <joseantonioug-09@hotmail.com>
2018-06-26 21:38:54 -0600

    I've added new functions in UI

--- app.js
+++ app.js
@@ -10,9 +10,27 @@ class Product
 
 class UI
 {
-	addProduct()
+	addProduct(product)
 	{
+		const productLIst = document.getElementById('product-list');
+		const element = document.createElement('div');
+		element.innerHTML = `
+			<div class="card text-center mb-4">
+				<div class="card-body">
+					<strong>Product</strong>: ${product.name}
+					<strong>Product Price</strong>: ${product.price}
+					<strong>Product Year</strong>: ${product.year}
+					<a href="#" class="btn btn-danger">Delete</a>
+				</div>
+			</div>
+		`;
 
+		productLIst.appendChild(element);
+	}
+
+	resetForm()
+	{
+		document.getElementById('product-form').reset();
 	}
 
 	deleteProduct()
@@ -28,7 +46,14 @@ class UI
 
 document.getElementById('product-form')
 	.addEventListener('submit', function(e) {
+		e.preventDefault();
 		var name = document.getElementById('name').value;
 		var price = document.getElementById('price').value;
 		var year = document.getElementById('year').value;
+
+		const product = new Product(name, price, year);
+		const ui = new UI();
+		ui.addProduct(product);
+		ui.resetForm();
+		
 	});
