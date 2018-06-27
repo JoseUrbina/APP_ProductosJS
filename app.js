5b3218b7c73427a27ab64a8b22c58396ac6bd0e2
JoseUrbina <joseantonioug-09@hotmail.com>
2018-06-26 22:16:18 -0600

    I've finished the APP

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ class UI
 					<strong>Product</strong>: ${product.name}
 					<strong>Product Price</strong>: ${product.price}
 					<strong>Product Year</strong>: ${product.year}
-					<a href="#" class="btn btn-danger">Delete</a>
+					<a href="#" class="btn btn-danger" name="delete">Delete</a>
 				</div>
 			</div>
 		`;
@@ -33,14 +33,30 @@ class UI
 		document.getElementById('product-form').reset();
 	}
 
-	deleteProduct()
+	deleteProduct(element)
 	{
-
+		if(element.name === 'delete')
+		{
+			element.parentElement.parentElement.parentElement.remove();
+			this.showMessage("Product deleted successfully","danger");
+		}
 	}
 
-	showMessage()
+	showMessage(message, cssClass)
 	{
+		const div = document.createElement('div');
+		div.className = `alert alert-${cssClass} mt-4`;
+		div.appendChild(document.createTextNode(message));
 
+		// Showing in DOM
+		const container = document.querySelector('.container');
+		const app = document.querySelector('#App');
+		container.insertBefore(div,app);
+
+		setTimeout(function()
+		{
+			document.querySelector(".alert").remove();
+		}, 3000);
 	}
 }
 
@@ -55,5 +71,12 @@ document.getElementById('product-form')
 		const ui = new UI();
 		ui.addProduct(product);
 		ui.resetForm();
-		
+		ui.showMessage("Product added successfully","success");
+	});
+
+document.getElementById('product-list')
+	.addEventListener('click',function(e)
+	{
+		const ui = new UI();
+		ui.deleteProduct(e.target);
 	});
